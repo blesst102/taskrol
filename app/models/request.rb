a732cfb701105f039038b315a4bf8378ec76a3ec
blesst102 <blesst102@gmail.com>
2021-11-21 22:28:25 +0000

    changes

--- app/models/request.rb
+++ app/models/request.rb
@@ -3,7 +3,7 @@ class Request < ApplicationRecord
   belongs_to :requestoption
   has_many :proposals
   belongs_to :user
-  #has_many :reviews
+
   validates :title, presence: true
   paginates_per 20
   
