5cc311fe4f045a663ca6deb5ac689dbf8c1cde45
Tademe Bless <43898001+blesst102@users.noreply.github.com>
2022-04-27 16:53:14 +0000

    Update production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -117,15 +117,15 @@ Rails.application.configure do
   # config.active_record.database_selector = { delay: 2.seconds }
   # config.active_record.database_resolver = ActiveRecord::Middleware::DatabaseSelector::Resolver
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
-  config.action_mailer.default_url_options = { :host => 'https://taskrol.com/' }
+  config.action_mailer.default_url_options = { :host => 'url' }
   config.action_mailer.default :charset => "utf-8"
   config.action_mailer.perform_deliveries = true
   config.action_mailer.delivery_method = :smtp
   ActionMailer::Base.smtp_settings = {
     :address     => 'smtp.gmail.com',
     :port     => 587,
-    :user_name     => "taskrolgh@gmail.com",
-    :password     => "cXn7rdq?TEwV",
+    :user_name     => "email",
+    :password     => "password",
     :authentication     => 'login',
     :enable_starttls_auto     =>  true
   }
